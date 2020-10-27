3b976bbfadc2c898eb971d8ed696e062b3642ac8
ishankhandekar <66933296+ishankhandekar@users.noreply.github.com>
2020-10-27 16:58:17 -0700

    I worked on for loop mastery

--- src/_01_algorithms/_1_prime_or_not/Prime_or_not.java
+++ src/_01_algorithms/_1_prime_or_not/Prime_or_not.java
@@ -3,21 +3,25 @@ package _01_algorithms._1_prime_or_not;
 import javax.swing.JOptionPane;
 
 public class Prime_or_not {
- public static void main(String[] args) {
-	String prime = JOptionPane.showInputDialog("Enter a number. I will tell you if it is prime or not.");
-	long prime2 = Integer.parseInt(prime);
-	
+	public static void main(String[] args) {
+		String prime = JOptionPane.showInputDialog("Enter a number. I will tell you if it is prime or not.");
+		long prime2 = Integer.parseInt(prime);}
+
 //	prime2 = prime2 / 2;
-	
-	
-	for (int i = 2; i < prime2; i++) {
-		
-		if (prime2 % i == 0) {
-			JOptionPane.showMessageDialog(null, "This is not prime");
-			System.exit(0);
+	static int[] factors(long prime2) {
+		for (int j = 0; j < prime2; j++) {
+			for (int i = 2; i < prime2; i++) {
+
+				if (prime2 % i == 0) {
+					//not prime
+					continue;
+				} else {
+					//is prime
+					
+				}
+			}}
+		return null;
 		}
-		
+
 	}
-	JOptionPane.showMessageDialog(null, "This is prime");
-}
-}
+
--- src/_02_nested_loops/_3_for_loop_gauntlet/ForLoop_Mastery.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/ForLoop_Mastery.java
@@ -37,8 +37,10 @@ public class ForLoop_Mastery {
 			for (int j = 0; j < 3; j++) {
 				System.out.println(i +" "+ j);
 			}
-			for (int j = 1; j < 10; j++) {
-				System.out.print(j);
+			
+		}for (int j = 1; j < 10; j++) {
+			for (int i = 0; i < 5; i++) {
+				
 			}
 		}
 	}
