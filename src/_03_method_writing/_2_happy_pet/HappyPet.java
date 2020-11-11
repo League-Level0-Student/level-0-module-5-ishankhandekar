2242ce09805b6d08b9a03bc0f337cf2b836258d4
ishankhandekar <66933296+ishankhandekar@users.noreply.github.com>
2020-11-10 16:59:56 -0800

    11/10/2020

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -1,30 +1,85 @@
 package _03_method_writing._2_happy_pet;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Time;
+
 import javax.swing.JOptionPane;
+import javax.swing.Timer;
 
 public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
+	static int happinessLevel= 0;
 	// this will be used to store the happiness of your pet
+	 static boolean trueFalse = false;
+	 Thread thread;
+	void code() {String pet = JOptionPane.showInputDialog("What kind of pet do you want to buy?");
+	// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 	
+	// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
+	//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
+	//    Make sure to customize the title and question too.
+	thread = new Thread(()->{Timer time = new Timer(60000000,(e)-> {HappyPet.setTrueFalse();});
+	time.start();
+	System.out.println("timer started");
+	}); 
+	thread.start();
+	
+	
+	while (trueFalse == true) {
+		
+	int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy?", "Happy Pet", 0, JOptionPane.INFORMATION_MESSAGE, null,
+			new String[] {"cuddle", "food", "water", "take a walk", "groom", "clean up poop"}, null);
+	if (task == 0) {
+		cuddle();
+	}if (task == 1) {
+		food();
+	}if (task == 2) {
+		water();
+	}if (task == 3) {
+		takeWalk();
+	}if (task == 4) {
+		groom();
+	}if (task == 5) {
+		cleanPoop();
+	} if (happinessLevel == 25) {
+		trueFalse = true;
+	}}
+}
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
-		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-		
-			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
-			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
-			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
-
-			// 5. Use user input to call the appropriate method created in step 4.
-
-			// 6. If you determine the happiness level is large enough, tell the
-			//    user that he loves his pet and use break; to exit for loop.
+	
+		HappyPet happyPet = new HappyPet();
+		happyPet.code();
 
 	}
 
 	// 4. Create methods to handle each of your user selections.
+	public static void cuddle() {
+		JOptionPane.showMessageDialog(null, "mrow");
+		happinessLevel = happinessLevel + 4;
+	}public static void food() {
+		JOptionPane.showMessageDialog(null, "mmmmm");
+		happinessLevel = happinessLevel + 5;
+	}public static void water() {
+		JOptionPane.showMessageDialog(null, "shhhhhhhhhhoooooooop");
+		happinessLevel = happinessLevel + 4;
+	}public static void takeWalk() {
+		JOptionPane.showMessageDialog(null, "meow");
+		happinessLevel = happinessLevel + 2;
+	}public static void groom() {
+		JOptionPane.showMessageDialog(null, "ahhh");
+		happinessLevel = happinessLevel + 3;
+	}public static void cleanPoop() {
+		JOptionPane.showMessageDialog(null, "(watches you)");
+				happinessLevel = happinessLevel + 1;
+	}
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+
+	static void setTrueFalse() {
+		trueFalse = false;
+		System.out.println("timer over");
+	}
+	
 }
---
+++ src/_04_practice/_1_skill_practice/SkillPractice.java
@@ -0,0 +1,59 @@
+package _04_practice._1_skill_practice;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class SkillPractice {
+	public static void main(String[] args) {
+
+		SkillPractice skills = new SkillPractice();
+		skills.skill1();
+		skills.skill2();
+		skills.skill3();
+		skills.skill4();
+		skills.skill5();
+	}
+
+	void skill1() {
+// Use pop-ups for the following.
+// Ask the user how many dimes they have
+		String dimes = JOptionPane.showInputDialog("how many dimes do you have?");
+
+// Tell them how many cents they have (hint multiply by 10)
+		int dimes2 = Integer.parseInt(dimes);
+		int dimes3 = dimes2 * 10;
+		JOptionPane.showMessageDialog(null, "you have " + dimes3 + " cents");
+
+// Ask the user how tall they are (inches)
+		String inches = JOptionPane.showInputDialog("How tall are you in inches?");
+		int inches2 = Integer.parseInt(inches);
+// If they are shorter than 36 inches, tell them to eat their Wheaties
+		if (inches2 < 36) {
+			JOptionPane.showMessageDialog(null, "EAT YOUR WHEATIES");
+		}
+
+	}
+
+	void skill2() {// Write a loop to print every third number between 1 and 30 to the console
+
+		for (int i = 1; i < 11; i++) {
+
+			System.out.println(3 * i);
+		}
+
+	}
+
+	void skill3() {// Write a random number less than 20
+		Random randy = new Random();
+		System.out.println(randy.nextInt(20));
+	}
+
+	void skill4() {// Write a loop to print every third number between 1 and 30 to the console
+
+	}
+
+	void skill5() {// Write a loop to print every third number between 1 and 30 to the console
+
+	}
+}
